5816c05b582164cb8a79b4efb7d4c10c99ff1ca0
Hyper-TH <twila.shayne.habab@outlook.com>
2023-11-15 14:21:19 +0000

    Added basic server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+// Create Express application
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Create a GET route
+// /message is an endpoint that returns JSON object with message
+app.get('/message', (req, res) => {
+    res.json({ message: 'Hello from server!' });
+});
+
+// Start server
+app.listen(8000, () => {
+    console.log(`Server is running on port 8000`);
+});
