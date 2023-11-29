97a750067ab1b131d5943144094d6b37b538e7d8
Hyper-TH <twila.shayne.habab@outlook.com>
2023-11-29 02:14:49 +0000

    Successfully render the passed params name and spc

--- src/pages/MedicinePage.js
+++ src/pages/MedicinePage.js
@@ -1,9 +1,24 @@
-export const MedicinePage = ({subPageName, backTo}) => {
+import { Link, useParams } from 'react-router-dom';
+
+export const MedicinePage = ({ subPageName, backTo }) => {
+    // const location = useLocation();
+
+    // const { medicineName, parsedSPC } = location.state || {};
+    const { medicineName, parsedSPC } = useParams();
+
+    console.log('Medicine name: ', medicineName);
+    
+    // console.log("Location State: ", location.state);
+
     return (
         <>
+            <h1>{subPageName} Page</h1>
+            <p>Medicine Name: {medicineName}</p>
+            <p>Parsed SPC: {parsedSPC}</p>
+
             <button>
                 <Link to={backTo}>Back to the Search Page</Link>
             </button>
         </>
-    )
+    );
 };
