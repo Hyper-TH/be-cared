8e0e3fc10e0ec473a45218f831eae2a1042c1555
Hyper-TH <twila.shayne.habab@outlook.com>
2023-12-04 02:13:26 +0000

    Added params (company, ingredient) and render within the page

--- src/pages/MedicinePage.js
+++ src/pages/MedicinePage.js
@@ -1,15 +1,15 @@
 import { Link, useParams, useNavigate } from 'react-router-dom';
 
 export const MedicinePage = ({ subPageName, backTo }) => {
-    const { medicineName, parsedSPC } = useParams();
+    const { medicineName, parsedSPC, company, activeIngredient } = useParams();
 
     const navigate = useNavigate();
 
-    const renderHTML = (medicineName, parsedSPC) => {
+    const renderHTML = (medicineName, parsedSPC, company, activeIngredient) => {
         console.log(parsedSPC);
         
         navigate({
-            pathname: `/render/${encodeURIComponent(medicineName)}/${encodeURIComponent(parsedSPC)}`
+            pathname: `/render/${encodeURIComponent(medicineName)}/${encodeURIComponent(parsedSPC)}/${encodeURIComponent(company)}/${activeIngredient}`
         });
     };
 
@@ -17,9 +17,11 @@ export const MedicinePage = ({ subPageName, backTo }) => {
         <>
             <h1>{subPageName} Page</h1>
             <p>Medicine Name: {medicineName}</p>
+            <p>Company: {company}</p>
+            <p>Active Ingredient: {activeIngredient}</p>
             <p>Parsed SPC: {parsedSPC}</p>
 
-            <button onClick={() => renderHTML(medicineName, parsedSPC)}>
+            <button onClick={() => renderHTML(medicineName, parsedSPC, company, activeIngredient)}>
                 View document
             </button>
             <button>
