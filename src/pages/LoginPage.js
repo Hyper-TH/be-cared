73adfc5883239aa146b94c4a9482d446c5c374b3
Hyper-TH <twila.shayne.habab@outlook.com>
2023-12-15 13:34:12 +0000

    Renamed to loginpage

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+// TODO: REUSE THIS PAGE TO SOMETHING ELSE!
+
+export const LoginPage = () => {
+    return (
+        <>
+            <div className=''>
+            </div>
+        </>
+    );
+};
+
