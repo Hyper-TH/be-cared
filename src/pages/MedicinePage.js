43d0ff7af670e71e85c99b345ca05ec7236eb5ef
Hyper-TH <twila.shayne.habab@outlook.com>
2023-12-17 17:55:16 +0000

    Added path to PIL render

--- src/pages/MedicinePage.js
+++ src/pages/MedicinePage.js
@@ -1,27 +1,41 @@
 import { Link, useParams, useNavigate } from 'react-router-dom';
 
 export const MedicinePage = ({ subPageName, backTo }) => {
-    const { medicineName, parsedSPC, company, activeIngredient } = useParams();
+    const { medicineName, parsedSPC, pil, company, activeIngredient } = useParams();
 
     const navigate = useNavigate();
 
-    const renderHTML = (medicineName, parsedSPC, company, activeIngredient) => {
+    const renderHTML = (medicineName, parsedSPC, pil, company, activeIngredient) => {
         console.log(parsedSPC);
         
         navigate({
-            pathname: `/render/${encodeURIComponent(medicineName)}/${encodeURIComponent(parsedSPC)}/${encodeURIComponent(company)}/${activeIngredient}`
+            pathname: `/render/${encodeURIComponent(medicineName)}/${encodeURIComponent(parsedSPC)}/${encodeURIComponent(pil)}/${encodeURIComponent(company)}/${activeIngredient}`
+        });
+    };
+
+    const renderPIL = (medicineName, parsedSPC, pil, company, activeIngredient) => {
+        console.log(parsedSPC);
+        
+        navigate({
+            pathname: `/pil/${encodeURIComponent(medicineName)}/${encodeURIComponent(parsedSPC)}/${encodeURIComponent(pil)}/${encodeURIComponent(company)}/${activeIngredient}`
         });
     };
 
     return (
         <>
             <h1>{medicineName}</h1>
-            <p>Company: {company}</p>
-            <p>Active Ingredient: {activeIngredient}</p>
-
-            <button onClick={() => renderHTML(medicineName, parsedSPC, company, activeIngredient)}>
+            <div>
+                <p>Company: {company}</p>
+                <p>Active Ingredient: {activeIngredient}</p>
+            </div>
+            <button onClick={() => renderHTML(medicineName, parsedSPC, pil, company, activeIngredient)}>
                 View SPC Document
             </button>
+
+            <button onClick={() => renderPIL(medicineName, parsedSPC, pil, company, activeIngredient)}>
+                View PIL Document
+            </button>
+
             <button>
                 <Link to={backTo}>Back to the Search Page</Link>
             </button>
