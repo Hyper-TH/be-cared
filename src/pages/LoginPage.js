e71867c4e578da4bc3c5de9daab7fc3315db3bae
Hyper-TH <twila.shayne.habab@outlook.com>
2024-02-03 20:58:55 +0000

    Modified so that login is a separate component

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,64 +1,13 @@
 import { useState,useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Axios from 'axios';
+import { SignIn } from './components/auth/SignIn';
 
 export const LoginPage = () => {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [response, setResponse] = useState("");
-
-    const login = async (e) => {
-        e.preventDefault(); // Prevents default form submission behaviour (refreshing)
-
-        try {
-            const res = await Axios.get(`http://localhost:8000/login?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`);
-
-            if (res.data.uid) {
-                setResponse(res.data.uid);
-    
-                console.log(response);
-
-                navigate({
-                    pathname: 
-                    `/home`,
-        
-                });
-            } else {
-                console.log("Incorrect login details");
-            }
-
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    useEffect(() => {
-        console.log(response);
-    }, [response]);
-
     return (
-        <div className="signInContainer">
-            <form onSubmit={login}>
-                <h1>Log In to your Account</h1>
-                <input
-                    type="email"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-
-                <input
-                    type="password"
-                    placeholder="Enter your password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                
-                <button type="submit">Log In</button>
-            </form>
-
+        <>
+        <div className="App">
+            <SignIn />
         </div>
+        </>
     );
 };
 
