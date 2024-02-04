0bed462996ee0a4f1cd5da0e8bc9e686a8e601cb
Hyper-TH <twila.shayne.habab@outlook.com>
2024-02-04 02:14:51 +0000

    Added comments

--- src/pages/components/AuthDetails.js
+++ src/pages/components/AuthDetails.js
@@ -1,11 +1,16 @@
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { useEffect, useState } from 'react'; 
+import { useNavigate } from 'react-router-dom';
 import { auth } from '../../config/config.js';
 
 export const AuthDetails = () => {
+    const navigate = useNavigate();
     const [authUser, setAuthUser] = useState(null);
 
+    // useEffect to listen for changes in authentication state
     useEffect(() => {
+
+        // Function to listen for changes in authentication state
         const listen = onAuthStateChanged(auth, (user) => {
             if (user) {
                 setAuthUser(user);
@@ -14,15 +19,18 @@ export const AuthDetails = () => {
             }
         });
 
+        // Cleanup function to remove the listener when the component unmounts
         return () => {
             listen();
         };
-    }, []);
+    }, []); // The empty dependency array ensures that this effect runs once when the component mounts
 
     const userSignOut = () => {
         signOut(auth) 
         .then(() => {
             console.log("Sign out successful");
+            navigate('/loggedOut');
+            
         })
         .catch((error) => console.log(error));
     };
