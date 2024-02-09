fd50d5f5ba36dbe34c8e19ebf6ac9ec86be3710f
Hyper-TH <twila.shayne.habab@outlook.com>
2024-02-09 03:16:01 +0000

    Moved page imports

---
+++ src/RouteImports.js
@@ -0,0 +1,19 @@
+/* GUEST */
+import GuestPage from './pages/guest/GuestPage.jsx';
+import GuestSearchPage from './pages/guest/GuestSearchPage.jsx';
+import GuestMedicinePage from './pages/guest/GuestMedicinePage.jsx';
+import GuestPDFRenderPage from './pages/guest/GuestPDFRenderPage.jsx';
+
+/* REGISTERED */
+import HomePage from './pages/HomePage.jsx';
+import LoginPage  from './pages/LoginPage.jsx';
+import MedicinePage  from './pages/MedicinePage.jsx';
+import SubscriptionsPage from './pages/SubscriptionsPage.jsx';
+import SearchPage  from './pages/SearchPage.jsx';
+import PDFRenderPage from './pages/PDFRenderPage.jsx';
+
+
+/* VERIFIED */
+import MerckPage  from './pages/MerckPage.jsx';
+
+export { GuestPage, GuestSearchPage, GuestMedicinePage, GuestPDFRenderPage, HomePage, LoginPage, MedicinePage, SubscriptionsPage, SearchPage, MerckPage, PDFRenderPage };
