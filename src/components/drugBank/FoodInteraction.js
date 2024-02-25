4256a5e3655567e43c259db6e294f446c5f2f544
Hyper-TH <twila.shayne.habab@outlook.com>
2024-02-25 15:56:39 +0000

    Completed TODO: appropriate rendering for 0 food interactions

--- src/components/drugBank/FoodInteraction.js
+++ src/components/drugBank/FoodInteraction.js
@@ -1,20 +1,27 @@
 export const FoodInteraction = (props) => {
+    const listItems = Object.entries(props.interactions).map(([key, value]) => {
+        return <li key={key}>{value}</li>;
+    });
 
-    const listItems = Object.entries(props.interactions).map(([key, value]) => (
-        <li key={key}>{value}</li>
-    ));
-
-    // TODO: Render properly for 0 interactions
     return (
         <div className="food_interaction">
-            <div>
-                Drug name: {props.name}
-                has {props.num_interactions}
+            {(props.num_interactions).includes("0") ? (
+                <>
+                <div>
+                    Drug name: {props.name} has 0 food interactions
+                </div>
+                </>
+            ) : (
+                <>
+                <div>
+                    Drug name: {props.name}
+                    has {props.num_interactions}
+                </div>
+                <div>
+                    Interactions: <ul>{listItems}</ul>
+                </div>
+                </>
+            )}
             </div>
-
-            <div>
-                Interactions: <ul>{listItems}</ul>
-            </div>
-        </div>
-    );
+        );
 };
