f494313c38de5231be17cbbe06c8ea6cdff065e1
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-06 22:06:39 +0000

    Successfully render product details and product properties

--- src/pages/merck/ProductPage.jsx
+++ src/pages/merck/ProductPage.jsx
@@ -3,7 +3,7 @@ import Axios from 'axios';
 import { useState, useEffect } from 'react';
 
 const ProductPage = ({ backTo }) => {
-    const [productDetails, setProductDetails] = useState({});
+    const [productInformation, setProductInformation] = useState({});
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState("");
 
@@ -12,28 +12,56 @@ const ProductPage = ({ backTo }) => {
     let state = location.state;
     let product = state.product;
 
-    const getProductDetails = async () => {
-        setIsLoading(true);
-        setError("");
-
-        try {
-            const response = await Axios.get(`http://localhost:8000/getProduct?uploadPath=${encodeURIComponent(product.href)}`);
-            
-            console.log(response.data);
-        } catch (error) {
-            console.error(`Axios Error: ${error}`);
-            setError("Local Server Error");
-        }
-    };
-
     useEffect(() => {
+        const getProductDetails = async () => {
+            setIsLoading(true);
+            setError("");
+    
+            try {
+                const response = await Axios.get(`http://localhost:8000/getProduct?uploadPath=${encodeURIComponent(product.href)}`);
+                
+                console.log(response.data.doc)
+                if (response.data) {
+                    setProductInformation(response.data.doc);
+                } else {
+                    setProductInformation({});
+                }
+    
+            } catch (error) {
+                console.error(`Axios Error: ${error}`);
+                setError("Local Server Error");
+            }
+        };
+
         getProductDetails();
     }, []);
+
+
 
     // TODO: Linear Formula Sub on Numbers
     return (
         <>
-            <h1>{product.productName}</h1>
+            <h1>{productInformation.productName}</h1>
+
+            <h2>{productInformation.productDescription}</h2>
+
+            <h3>Product Information</h3>    
+            <div>
+            {Object.entries(productInformation?.productDetails ?? {}).map(([key, value]) => (
+                <div key={key}>
+                <strong>{key}</strong>: {value}
+                </div>
+            ))}
+            </div>
+
+            <h3>Product Properties</h3>
+            <div>
+            {Object.entries(productInformation?.productProperties ?? {}).map(([key, value]) => (
+                <div key={key}>
+                <strong>{key}</strong>: {value}
+                </div>
+            ))}
+            </div>
 
             <button>
                 <Link to={backTo}>Back to the Search Page</Link>
