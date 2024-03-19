1d904578465b961afe51b875ddf2953b2482f48b
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-19 18:04:53 +0000

    Fixed class warnings

--- src/pages/drugBank/SearchDrugPage.jsx
+++ src/pages/drugBank/SearchDrugPage.jsx
@@ -179,7 +179,7 @@ const SearchDrugPage = ({ backTo }) => {
 						</div>
 				<div className="no_interactions_warning">
 					<div>
-						<svg class="info" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+						<svg className="info" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
 							<path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
 						</svg>
 						<b>Warning</b>: If no interactions are found between two drugs, it does not
@@ -235,7 +235,7 @@ const SearchDrugPage = ({ backTo }) => {
 			{error && (
 				<div className="error">
 				<svg
-					class="flex-shrink-0 inline w-4 h-4 me-3"
+					className="flex-shrink-0 inline w-4 h-4 me-3"
 					aria-hidden="true"
 					xmlns="http://www.w3.org/2000/svg"
 					fill="currentColor"
