2c87e24519e2d6235bd2e415298a545f4f300935
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-19 14:05:42 +0000

    Added remove button for drugs

--- src/components/drugBank/Drug.js
+++ src/components/drugBank/Drug.js
@@ -1,8 +1,13 @@
+import  '../../styles/drugbankPages/drug_interactions.css';
+
 export const Drug = (props) => {
     return (
-        <div className="drug">
-            <p>{props.drugName}</p>
-            <button onClick={() => props.deleteDrug(props.drugId)}>X</button>
-        </div>
+        <button className="drug_button" onClick={() => props.deleteDrug(props.drugId)}>
+            <svg class="remove_drug" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z"/>
+            </svg>
+            
+            {props.drugName}
+        </button>
     );
 };
