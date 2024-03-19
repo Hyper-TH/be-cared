d6c05a3f1cc1d70d9d332c970ce11b44b941dc75
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-19 19:13:40 +0000

    Fixed class warning

--- src/components/drugBank/Drug.js
+++ src/components/drugBank/Drug.js
@@ -3,7 +3,7 @@ import  '../../styles/drugbankPages/drug_interactions.css';
 export const Drug = (props) => {
     return (
         <button className="drug_button" onClick={() => props.deleteDrug(props.drugId)}>
-            <svg class="remove_drug" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <svg className="remove_drug" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z"/>
             </svg>
             
