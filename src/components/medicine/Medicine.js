c8d718f3099eb54995296338de3f9c693795262c
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-20 19:56:39 +0000

    Added style for new document notifications

--- src/components/medicine/Medicine.js
+++ src/components/medicine/Medicine.js
@@ -1,12 +1,26 @@
 export const Medicine = (props) => {
     const hasPil = props.medicine.pil.available;
     const hasSpc = props.medicine.spc.available;
-    // const pilUpdate = props.medicine.pil.notifications;
-    // const spcUpdate = props.medicine.spc.notifications;
+    const pilUpdate = props.medicine.pil.notifications;
+    const spcUpdate = props.medicine.spc.notifications;
 
-    console.log(props.medicine.activeIngredients);
+    console.log(spcUpdate);
 
-    const spcButton = hasSpc ? (
+    const spcButton = hasSpc  && spcUpdate ? (
+        <button 
+            className="btn_collection_med_left inline-flex items-center px-5 py-2.5"
+            onClick={() => props.renderSPC(props.medicine)}>
+            View SPC Document
+
+            <span className="new_document">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                    <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
+                </svg>
+            </span>
+
+        </button>
+        
+    ) : hasSpc ? (
         <button 
             className="btn_collection_med_left"
             onClick={() => props.renderSPC(props.medicine)}>
@@ -21,7 +35,20 @@ export const Medicine = (props) => {
         </button>
     );
 
-    const pilButton = hasPil ? (
+    const pilButton = hasPil && pilUpdate? (
+        <button 
+            className='btn_collection_med_mid inline-flex items-center px-5 py-2.5' 
+            onClick={() => props.renderPIL(props.medicine)}>
+            View PIL Document
+
+            <span className="new_document">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                    <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
+                </svg>
+            </span>
+            
+        </button>
+    ) : hasPil ? (
         <button 
             className='btn_collection_med_mid' 
             onClick={() => props.renderPIL(props.medicine)}>
