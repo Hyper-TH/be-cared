e9a5a9fb00ac53b260f930d96d6f63827f4c6c43
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-21 19:32:55 +0000

    Added flowbite to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export const content = [
   "./src/**/*.{js,jsx,ts,tsx}",
+  "./node_modules/flowbite/**/*.js",
+  "node_modules/flowbite-react/lib/esm/**/*.js",
 ];
 
 export const theme = {
