f03ea26f62c591917ec7f5d617b560afc74a00ef
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-21 20:13:24 +0000

    Established search

--- src/pages/merck/SearchProductPage.jsx
+++ src/pages/merck/SearchProductPage.jsx
@@ -4,6 +4,7 @@ import '../../styles/merck/search_product.css';
 import Axios from 'axios';
 import Product from '../../components/merck/Product';
 
+// TODO: Error when no type is selected
 const SearchProductPage = ({ backTo }) => {
     const [prodQuery, setProdQuery] = useState("");     // State for product query to send to server
     const [productList, setProductList] = useState([])  // State for list of product responses
@@ -26,6 +27,9 @@ const SearchProductPage = ({ backTo }) => {
         setError("");
 
         if (prodQuery && searchType) {
+            console.log(`Search Type: `, searchType);
+            console.log(`Searching:` , prodQuery);
+            
             try {
                 console.log(`Requesting server for product list..`);
                 const response = await Axios.get(
@@ -77,10 +81,10 @@ const SearchProductPage = ({ backTo }) => {
                                 <div className='search_container'>
 
                                     <div className="relative flex-2">
-                                        <select id="type" className="search_type">
-                                            <option selected>Search Type</option>
-                                            <option value="US">Product ID</option>
-                                            <option value="DE">Product Name</option>
+                                        <select id="dropdown" value={searchType} className="search_type" onChange={handleDropdownChange}>
+                                            <option value="">Search Type</option>
+                                            <option value="number">Product ID</option>
+                                            <option value="name">Product Name</option>
 
                                         </select>
                                         
@@ -93,9 +97,9 @@ const SearchProductPage = ({ backTo }) => {
                                     </div>
                         
                                     <div className="relative flex-1">
-                                        <input type="search" className="product_input" placeholder="T1503 or Trizma Base..." required />
+                                        <input type="search" value={prodQuery} onChange={productChange} className="product_input" placeholder="T1503 or Trizma Base..." required />
                                         
-                                        <button type="submit" className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                        <button onClick={searchProduct} className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                             <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                             </svg>
