bb17d88f075eddedf3ccc86ef84e319f69dc483b
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-21 20:01:18 +0000

    Established style of dropdown search

--- src/pages/merck/SearchProductPage.jsx
+++ src/pages/merck/SearchProductPage.jsx
@@ -79,136 +79,29 @@ const SearchProductPage = ({ backTo }) => {
 
                 <div className='search_product_container'>
                     <div className='search_product'>
+                        <div className='search_product_bar'>
+                            <form className="search_product_form">
+                                <div className='search_container'>
 
-                        
-                    <form className="max-w-sm mx-auto relative">
-                    <label htmlfor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-                    <div className="relative">
-                        <select id="countries" className="appearance-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-3 pr-10 py-2 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                            <option selected>Choose a country</option>
-                            <option value="US">United States</option>
-                            <option value="CA">Canada</option>
-                            <option value="FR">France</option>
-                            <option value="DE">Germany</option>
-                        </select>
-                        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-3 text-white">
-                            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 12">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 8 8 8-8"/>
-                            </svg>
-                        </div>
-                    </div>
-                </form>
+                                    <div className="relative flex-2">
+                                        <select id="type" className="search_type">
+                                            <option selected>Search Type</option>
+                                            <option value="US">Product ID</option>
+                                            <option value="DE">Product Name</option>
 
-                        
-
-                        <div className="max-w-lg mx-auto">
-                            <div className="flex">
-
-                                {/* <button data-dropdown-toggle="dropdown" className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" onClick={handleOpen}>
-                                    Search Type
-                                    <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
-                                    </svg>
-                                </button>
-                                    
-                                <div className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                                    {isOpen ? (
-                                        <ul className="dropdown_list" aria-labelledby="btn_dropdown">
-                                            <li>
-                                                <button className='btn_dropdown_option'>Product ID</button>
-                                            </li>
-
-                                            <li>
-                                                <button className='btn_dropdown_option'>Product Name</button>
-                                            </li>
-                                        </ul>
-                                    ) : null}
-
-                                </div> */}
-                                {/* <Dropdown className="btn_dropdown" dismissOnClick={false}>
-                                    <Dropdown.Item className='btn_dropdown_option'>Product ID</Dropdown.Item>
-                                    <Dropdown.Item className='btn_dropdown_option'>Product Type</Dropdown.Item>
-                                </Dropdown> */}
-
-                                                            
-                                <div className="relative w-full">
-                                    <input className="product_input" placeholder="T1503 or Trizma" />
-                                    <button className="product_search">
-                                        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                        </svg>
-                                        <span className="sr-only">Search</span>
-                                    </button>
-                                </div>
-
-                            </div>
-                        </div>
-                        <form className="max-w-lg mx-auto">
-                            <div className="flex">
-                                <label htmlfor="search-dropdown" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
-                                
-                                <button 
-                                    id="dropdown-button" 
-                                    data-dropdown-toggle="dropdown" 
-                                    className="yokeToCopy" type="button">All categories 
-                                    <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
-                                    </svg>
-                                </button>
-                                
-                                <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                                    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
-                                    <li>
-                                        <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mockups</button>
-                                    </li>
-                                    <li>
-                                        <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Templates</button>
-                                    </li>
-                                    <li>
-                                        <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Design</button>
-                                    </li>
-                                    <li>
-                                        <button type="button" className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logos</button>
-                                    </li>
-                                    </ul>
-                                </div>
-                                
-                                <div className="relative w-full">
-                                    <input type="search" id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required />
-                                    <button type="submit" className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                        <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                        </svg>
-                                        <span className="sr-only">Search</span>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-
-                        <div className='search_bar'>
-                            <form className="max-w-lg mx-auto">
-                                <div className='flex'>
-
-                                    {/* <form className="max-w-sm mx-auto relative"> */}
-                                        {/* If relative div is uncommented, the arrow disappears */}
-                                        <div className="relative">
-                                            <select id="countries" className="selectYoke">
-                                                <option selected>Search Type</option>
-                                                <option value="US">Product ID</option>
-                                                <option value="DE">Product Name</option>
-                                            </select>
-                                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-3 text-white">
-                                                <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 12">
-                                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 8 8 8-8"/>
-                                                </svg>
-                                            </div>
-                                        </div>
-                                    {/* </form> */}
-
+                                        </select>
                                         
+                                        <div className="dropdown_icon">
+                                            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 12">
+                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 8 8 8-8"/>
+                                            </svg>
+                                        </div>
+
+                                    </div>
                         
-                                    <div className="relative w-full">
-                                        <input type="search" id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required />
+                                    <div className="relative flex-1">
+                                        <input type="search" className="product_input" placeholder="T1503 or Trizma Base..." required />
+                                        
                                         <button type="submit" className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                             <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
