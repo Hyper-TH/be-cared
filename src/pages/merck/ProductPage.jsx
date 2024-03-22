a20fe2ccb5d76d20843702ae6751a6fbbe71cacd
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-22 02:37:32 +0000

    Implement tailwind for product details

--- src/pages/merck/ProductPage.jsx
+++ src/pages/merck/ProductPage.jsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 import Axios from 'axios';
 import { useState, useEffect } from 'react';
+import '../../styles/merck/product.css';
 
 const ProductPage = ({ backTo }) => {
     const [productInformation, setProductInformation] = useState({});
@@ -57,48 +58,62 @@ const ProductPage = ({ backTo }) => {
     // TODO: Linear Formula Sub on Numbers (refer to drug interactions extended description)
     return (
         <>
-        <div className="product_information">
-            {isLoading ? (
-                <div>Loading...</div>
-            ) : (
-                <>
-                <div className='product_name'>
-                    <h1>{productInformation.productName}</h1>
-                </div>
+        <section className='main_container'>
+            <div className='sub_container'>
+                <Link to={backTo}>
+                    <button className='btn_primary'>
+                        &larr;
+                    </button>
+                </Link>
 
-                <div className='product_description'>
-                    <h2>{productInformation.productDescription}</h2>
-                </div>
-
-                {/* <h3>Linear Formula: </h3> {formatLinearFormula(productInformation.linearFormula)}; */}
-
-                <div className='product_details'>
-                    {Object.entries(productInformation?.productDetails ?? {}).map(([key, value]) => (
-                        <div key={key}>
-                        <strong>{key}</strong>: {value}
-                        </div>
-                    ))}
-                </div>
-
-                <div className='product_properties'>
-                    <h3>Product Properties</h3>
-                    <div className='product_properties_list'>
-                        {Object.entries(productInformation?.productProperties ?? {}).map(([key, value]) => (
-                            <div key={key}>
-                                <strong>{key}</strong>: {value}
+                <div className='product_details_container'>
+                    <div className='product_details'>
+                        {isLoading ? (
+                            <div className='loading'>Loading...</div>
+                        ) : (
+                            <>
+                            <div className='product_name'>
+                                <h1>{productInformation.productName}</h1>
                             </div>
-                        ))}
+
+                            <div className='product_sub_details'>
+                                {Object.entries(productInformation?.productDetails ?? {}).map(([key, value]) => (
+                                    <div key={key}>
+                                        <span className='product_header inline'>{key} </span>
+                                        <span className='product_text inline'>{value}</span>
+                                    </div>
+                                ))}
+                            </div>
+
+                            <div className='product_sub_details'>
+                                <div className='product_properties_list'>
+                                    {Object.entries(productInformation?.productProperties ?? {}).map(([key, value]) => (
+                                        <div key={key}>
+                                            <span className='product_header inline'>{key}: </span>
+                                            <span className='product_text inline'>{value}</span>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                            </>
+                        )}
+
                     </div>
                 </div>
-                </>
-            )}
-        </div>    
+                
+            </div>
+                {error && 
+                    <div className='error'>
+                    <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                    </svg>
 
-        <button>
-            <Link to={backTo}>Back to the Search Page</Link>
-        </button>
-
-        {error && <p style={{ color: "red" }}>{error}</p>}
+                        <div>
+                            <span className='font-medium'>{error}</span>
+                        </div>
+                    </div>
+                }
+        </section>
         </>
     )
 };
