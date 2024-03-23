ce326a31eb3e7915c213ed38da39e1903e3d9cc5
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-23 17:23:55 +0000

    Mobile tailwind

--- src/components/drugBank/DrugInteraction.js
+++ src/components/drugBank/DrugInteraction.js
@@ -118,24 +118,24 @@ export const DrugInteraction = (props) => {
 
             <div className='interactions_row'>
                 
-                <div className='interactions_col subject'>
-                    {props.subject}
-                </div>
+                <div className='small_screen disable_small_screen:contents'>
+                    <div className='interactions_col subject'>
+                        {props.subject}
+                    </div>
 
-                <div className='interactions_col interacts'>
-                    <div className='interacts_icon'>
+                    <div className='interactions_col interacts'>
+                        <div className='interacts_icon'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
+                            </svg>
+                        </div> 
+                    </div>
 
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5" />
-                        </svg>
-
+                    <div className='interactions_col affected'>
+                        {props.affected}
                     </div>
                 </div>
-                
-                <div className='interactions_col affected'>
-                    {props.affected}
-                </div> 
-                
+                    
                 <div className='interactions_col severity'>
                     {severity}
                 </div>
