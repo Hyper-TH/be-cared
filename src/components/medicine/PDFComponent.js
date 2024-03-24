242d72c211fb80a8a0a450d7707fbc524d037022
Twila [Hyper] Habab <twila.shayne.habab@outlook.com>
2024-03-24 00:36:50 +0000

    Fixed warnings

--- src/components/medicine/PDFComponent.js
+++ src/components/medicine/PDFComponent.js
@@ -53,7 +53,7 @@ export const PDFComponent = ({ PDFURL }) => {
                     onClick={zoomOut} 
                     className="nav_left"
                     disabled={scale <= 0.2}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 m-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 m-auto">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
                     </svg>
                 </button>
@@ -61,7 +61,7 @@ export const PDFComponent = ({ PDFURL }) => {
                 <button 
                     onClick={zoomIn}
                     className="nav_mid">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 m-auto">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 m-auto">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                 </button>
